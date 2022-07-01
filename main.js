7b5c74de94b7d6e180d3259ca475ea49375ff470
AvniKhurania <89643274+AvniKhurania@users.noreply.github.com>
2022-07-01 19:54:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,42 @@
+function setup()
+{
+    video=createCapture(VIDEO);
+    video.size=500,500;
+    canvas=createCanvas(500,500);
 
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+
+} 
+
+
+
+
+function modelLoaded()
+{
+    console.log("PoseNet is initialized");
+    
+}
+
+
+function gotPoses(results)
+{
+    if(results.lenght>0)
+    {
+        console.log(results);
+        nosex=result[0].pose.nose.x;
+        nosey=results[0].pose.nose.y;
+        console.log("nosex="+nosex+"nosey="+nosey)
+
+        leftWristx=results[0].pose.leftWrist.x;
+        rightWristx=results[0].pose.rightWrist.x;
+
+        difference=floor(leftWristx-rightWristx);
+        console.log("leftWristx="+leftWristx+"rightWristx="+rightWristx+"difference="+difference);
+
+        
+    }
+    
+
+}
 
